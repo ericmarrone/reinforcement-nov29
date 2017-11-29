c41ddb17fc369c4bf43471d614ff674cf0194cee
Eric Marrone <emarrone17@gmail.com>
2017-11-29 13:41:51 -0500

    Fix error for numbers ending in 11,12,13

--- exercise.rb
+++ exercise.rb
@@ -1,19 +1,19 @@
 def ordinal_indicator(num)
   if num <= 0
     return num
-  elsif num.digits.first == 1 && num != 11
+  elsif num.digits.first == 1 && num.digits[-2] != 1
     return "#{num}st"
-  elsif num.digits.first == 2 && num != 12
+  elsif num.digits.first == 2 && num.digits[-2] != 1
     return "#{num}nd"
-  elsif num.digits.first == 3 && num != 13
+  elsif num.digits.first == 3 && num.digits[-2] != 1
     return "#{num}rd"
   else
     return "#{num}th"
   end
 end
 
-(0..100).each do |num|
-  puts ordinal_indicator(num)
-end
+# (0..113).each do |num|
+#   puts ordinal_indicator(num)
+# end
 
- # puts ordinal_indicator(-1)
+ puts ordinal_indicator(211ru)
